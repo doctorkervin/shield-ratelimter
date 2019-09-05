03cb7214ff655119133074fea0f56ec1bcb9e9bd
kervin <doctorkervin@Gmail.com>
2019-09-05 17:05:38 +0800

    @Kervin--将Controller 注解换成RestController

--- shleld-ratelimter-demo/src/main/java/com/snowalker/ratelimiter/demo/controller/TestController.java
+++ shleld-ratelimter-demo/src/main/java/com/snowalker/ratelimiter/demo/controller/TestController.java
@@ -4,9 +4,8 @@ import com.snowalker.shield.ratelimiter.core.annotation.RateLimiter;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author snowalker
@@ -15,14 +14,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * @className
  * @desc
  */
-@Controller
+@RestController
 public class TestController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
 
     private static final String MESSAGE = "{\"code\":\"400\",\"msg\":\"FAIL\",\"desc\":\"触发限流\"}";
 
-    @ResponseBody
     @RequestMapping("ratelimiter")
     @RateLimiter(key = "ratedemo:1.0.0", limit = 5, expire = 10, message = MESSAGE)
     public String sendPayment(HttpServletRequest request) throws Exception {
@@ -30,7 +28,6 @@ public class TestController {
         return "正常请求";
     }
 
-    @ResponseBody
     @RequestMapping("ratelimiter1")
     @RateLimiter(key = "ratedemo:1.0.1", limit = 5, expire = 10, message = MESSAGE)
     public String sendPayment1(HttpServletRequest request) throws Exception {
